6cf49d64af0562084c3c6902d7a844e3dbcb9d46
unknown <min.zhou@RSMYJDT0023.group.redstar.corp>
2017-07-24 16:45:56 +0800

    add folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+
+import './styles/main.scss';
+
+let imgData = require('./data/imgData.json');
+
+// 生成图片路径信息
+imgData = ((imgDataArr) => {
+	for(let i=0, len=imgDataArr.length; i<len; i++){
+		let singleImageData = imgDataArr[i];
+
+		singleImageData.imageURL = require(`images/${singleImageData.fileName}`);
+
+		imgDataArr[i] = singleImageData;
+	}
+})(imgData);
 
 class HelloReact extends React.Component {
 	render(){
