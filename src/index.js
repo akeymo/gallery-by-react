1c237d4921e06d635e7ac44bf29af7171bc12ce4
unknown <min.zhou@RSMYJDT0023.group.redstar.corp>
2017-07-25 10:33:46 +0800

    build stage

--- src/index.js
+++ src/index.js
@@ -3,27 +3,33 @@ import { render } from 'react-dom';
 
 import './styles/main.scss';
 
-let imgData = require('./data/imgData.json');
+let imgDatas = require('./data/imgData.json');
 
 // 生成图片路径信息
-imgData = ((imgDataArr) => {
-	for(let i=0, len=imgDataArr.length; i<len; i++){
-		let singleImageData = imgDataArr[i];
+imgDatas = ((imgDatasArr) => {
+	for(let i=0, len=imgDatasArr.length; i<len; i++){
+		let singleImageData = imgDatasArr[i];
 
-		singleImageData.imageURL = require(`images/${singleImageData.fileName}`);
+		singleImageData.imageURL = require(`./images/${singleImageData.fileName}`);
 
-		imgDataArr[i] = singleImageData;
+		imgDatasArr[i] = singleImageData;
 	}
-})(imgData);
 
-class HelloReact extends React.Component {
+	return imgDatasArr;
+
+})(imgDatas);
+
+class GalleryByReactApp extends React.Component {
 	render(){
 		return (
-			<div>
-				<h1>Hello React!</h1>
-			</div>
+			<section className="stage">
+				<section className="img-sec">
+				</section>
+				<nav className="controller-nav">
+				</nav>
+			</section>
 		)
 	}
 }
 
-render(<HelloReact />, document.getElementById('app'));
+render(<GalleryByReactApp />, document.getElementById('app'));
